0b9d5fc556ea23f58bcb90f7f3bf01e1c49f07cd
XyperCode <xypercode@ultreon.dev>
2024-12-02 09:15:25 +0100

    🚸 Make the binding button a little smaller.

--- common/src/main/java/dev/ultreon/controllerx/config/gui/BindingsList.java
+++ common/src/main/java/dev/ultreon/controllerx/config/gui/BindingsList.java
@@ -89,7 +89,7 @@ public class BindingsList extends ContainerObjectSelectionList<BindingsList.List
         private ListEntry(BindingsList list, Config config, ConfigEntry<?> configEntry, int rowWidth) {
             this.list = list;
             this.configEntry = (ControllerBindingEntry<?>) configEntry;
-            this.widget = configEntry.createButton(config, rowWidth - 160, 0, 150);
+            this.widget = configEntry.createButton(config, rowWidth - 110, 0, 100);
 
             this.resetBtn = new ImageButton(0, 0, 20, 20, 0, 0, 20, ControllerX.res("textures/gui/reset.png"), 20, 40, button -> {
                 configEntry.reset();
